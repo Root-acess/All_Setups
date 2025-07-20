ab49b0a8b87383ec8b1bfd24e3de7287bed759e3
Hiralal Singh <135902087+Root-acess@users.noreply.github.com>
2025-07-20 13:52:32 +0530

    Update Amazonwebsite.sh

--- Amazonwebsite.sh
+++ Amazonwebsite.sh
@@ -7,4 +7,4 @@ cd /var/www/html/
 rm index.nginx-debian.html 2>/dev/null  # optional: remove default file
 git clone https://github.com/Root-acess/amazon_clone.git
 mv amazon_clone/* .
-rm -rf amazon_clone  # clean up folder
+
